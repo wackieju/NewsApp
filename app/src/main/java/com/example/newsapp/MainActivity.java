115e08031e847c057928411a9b6566377e93fdea
wackieju <jiajie.wu96@gmail.com>
2019-07-26 03:49:02 -0700

    Added loader logic

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -1,28 +1,77 @@
 package com.example.newsapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.Loader;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.LoaderManager;
+import android.content.Context;
 import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements androidx.loader.app.LoaderManager.LoaderCallbacks<List<News>> {
+    private final int EQ_LOADER_ID = 1;
 
     private String dummyKey = "https://content.guardianapis.com/search?q=debate&tag=politics/politics&from-date=2014-01-01&api-key=test";
+
+    private TextView mEmptyListTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mEmptyListTextView = (TextView) findViewById(R.id.no_earthquakes_text_view);
+
         RecyclerView newsRecycler = (RecyclerView) findViewById(R.id.news_recycler);
         NewsRecyclerAdapter newsRecyclerAdapter = new NewsRecyclerAdapter(new ArrayList<News>());
 
         newsRecycler.setAdapter(newsRecyclerAdapter);
 
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // Get a reference to the LoaderManager, in order to interact with loaders.
+            LoaderManager loaderManager = getLoaderManager();
+
+            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+            // because this activity implements the LoaderCallbacks interface).
+            loaderManager.initLoader(EQ_LOADER_ID, null, this);
+        } else {
+            // Otherwise, display error
+            // First, hide loading indicator so error message will be visible
+            View loadingIndicator = findViewById(R.id.progressBar);
+            loadingIndicator.setVisibility(View.GONE);
+
+            // Update empty state with no connection error message
+            mEmptyListTextView.setText(R.string.no_internet_connection);
+        }
+        getLoaderManager().initLoader(EQ_LOADER_ID, null, this);
+    }
+
+
+    @NonNull
+    @Override
+    public Loader<List<News>> onCreateLoader(int id, @Nullable Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<List<News>> loader, List<News> data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<List<News>> loader) {
 
     }
 }
---
+++ app/src/main/java/com/example/newsapp/NewsLoader.java
@@ -0,0 +1,31 @@
+package com.example.newsapp;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.loader.content.AsyncTaskLoader;
+
+import java.util.List;
+
+public class NewsLoader extends AsyncTaskLoader<List<News>> {
+    private String mUrl;
+    public NewsLoader(@NonNull Context context, String Url) {
+        super(context);
+        mUrl = Url;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    @Nullable
+    @Override
+    public List<News> loadInBackground() {
+        if(mUrl == null){
+            return null;
+        }
+        return null;
+    }
+}
